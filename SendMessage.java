a7dc7cd09cbe063c0a759cfe444de0a38c84a474
Allison Reel <areel2003@gmail.com>
2024-01-19 12:14:46 -0500

    Create SendMessage.java

---
+++ SendMessage.java
@@ -0,0 +1,9 @@
+public class SendMessage {
+  public static void main(String[] args) {
+    for (int i=0; i < args.length; i++) {
+      String valentine = args[i];
+    System.out.println("Happy Valentine's Day, " + valentine + "!");
+      }
+  }
+  
+}
